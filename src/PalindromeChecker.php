027ff9da76c5a0cc443b1766c71af70adbb4109e
Sasha Laurent <sasha-laurent@hotmail.fr>
2021-07-11 19:45:02 +0200

    Init palindrome

--- src/PalindromeChecker.php
+++ src/PalindromeChecker.php
@@ -9,17 +9,6 @@ class PalindromeChecker
      * read it from left to right or from right to left) and false otherwise.
      */
     public function check(string $w): bool {
-        $tab = str_split($w);
-        $n = strlen($w);
-
-        for ($i = 0; $i <= floor($n/2); $i++) {
-            if ($tab[$i] == $tab[$n-$i-1]) {
-                continue;
-            } else {
-                return false;
-            }
-        }
-
         return true;
     }
 }
